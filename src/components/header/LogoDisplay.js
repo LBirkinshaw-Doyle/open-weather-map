aad21b8cc5dc1c51fe3f60ae33140b6fb69e2320
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2022-09-30 10:34:36 +0100

    add logos to LogoDisplay with new file to manage logos

--- src/components/header/LogoDisplay.js
+++ src/components/header/LogoDisplay.js
@@ -1,5 +1,35 @@
+import { logos_1x as logos } from '../logos';
+
 function LogoDisplay() {
-  return <div id="logo" />;
+  return (
+    <div id="logo">
+      <img
+        src={logos.logo01_1x}
+        alt="sunny"
+        className="logo active slide-in"
+      ></img>
+      <img
+        src={logos.logo02_1x}
+        alt="few clouds"
+        className="logo inactive"
+      ></img>
+      <img
+        src={logos.logo03_1x}
+        alt="scattered clouds"
+        className="logo inactive"
+      ></img>
+      <img src={logos.logo04_1x} alt="cloudy" className="logo inactive"></img>
+      <img
+        src={logos.logo09_1x}
+        alt="light rain"
+        className="logo inactive"
+      ></img>
+      <img src={logos.logo10_1x} alt="rain" className="logo inactive"></img>
+      <img src={logos.logo11_1x} alt="storm" className="logo inactive"></img>
+      <img src={logos.logo13_1x} alt="snow" className="logo inactive"></img>
+      <img src={logos.logo50_1x} alt="mist" className="logo inactive"></img>
+    </div>
+  );
 }
 
 export default LogoDisplay;
---
+++ src/components/logos.js
@@ -0,0 +1,45 @@
+import logo01_1x from './media/weather-icons/1x/01d.png';
+import logo02_1x from './media/weather-icons/1x/02d.png';
+import logo03_1x from './media/weather-icons/1x/03d.png';
+import logo04_1x from './media/weather-icons/1x/04d.png';
+import logo09_1x from './media/weather-icons/1x/09d.png';
+import logo10_1x from './media/weather-icons/1x/10d.png';
+import logo11_1x from './media/weather-icons/1x/11d.png';
+import logo13_1x from './media/weather-icons/1x/13d.png';
+import logo50_1x from './media/weather-icons/1x/50d.png';
+
+import logo01_2x from './media/weather-icons/2x/01d.png';
+import logo02_2x from './media/weather-icons/2x/02d.png';
+import logo03_2x from './media/weather-icons/2x/03d.png';
+import logo04_2x from './media/weather-icons/2x/04d.png';
+import logo09_2x from './media/weather-icons/2x/09d.png';
+import logo10_2x from './media/weather-icons/2x/10d.png';
+import logo11_2x from './media/weather-icons/2x/11d.png';
+import logo13_2x from './media/weather-icons/2x/13d.png';
+import logo50_2x from './media/weather-icons/2x/50d.png';
+
+const logos_1x = {
+  logo01_1x,
+  logo02_1x,
+  logo03_1x,
+  logo04_1x,
+  logo09_1x,
+  logo10_1x,
+  logo11_1x,
+  logo13_1x,
+  logo50_1x,
+};
+
+const logos_2x = {
+  logo01_2x,
+  logo02_2x,
+  logo03_2x,
+  logo04_2x,
+  logo09_2x,
+  logo10_2x,
+  logo11_2x,
+  logo13_2x,
+  logo50_2x,
+};
+
+export { logos_1x, logos_2x };
