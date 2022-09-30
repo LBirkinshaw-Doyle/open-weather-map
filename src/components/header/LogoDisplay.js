1b2e572db95e0937d708d6b8c30c26bf528a7232
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2022-09-30 11:37:49 +0100

    add animated weather logo

--- src/components/header/LogoDisplay.js
+++ src/components/header/LogoDisplay.js
@@ -1,33 +1,28 @@
+import { useEffect, useState } from 'react';
 import { logos_1x as logos } from '../logos';
+import './LogoDisplay.css';
 
 function LogoDisplay() {
+  const [logoIndex, updateLogoIndex] = useState(0);
+  const [logoArray] = useState(Object.values(logos));
+
+  useEffect(() => {
+    const AnimateLogoInterval = setInterval(animateLogo, 10000);
+    function animateLogo(logoImages) {
+      const randomIndex = Math.floor(Math.random() * 9);
+      updateLogoIndex(randomIndex);
+    }
+    return () => clearInterval(AnimateLogoInterval);
+  });
+
   return (
     <div id="logo">
       <img
-        src={logos.logo01_1x}
-        alt="sunny"
-        className="logo active slide-in"
+        src={logoArray[logoIndex]}
+        className="logo slide-in"
+        alt=""
+        key={logoIndex}
       ></img>
-      <img
-        src={logos.logo02_1x}
-        alt="few clouds"
-        className="logo inactive"
-      ></img>
-      <img
-        src={logos.logo03_1x}
-        alt="scattered clouds"
-        className="logo inactive"
-      ></img>
-      <img src={logos.logo04_1x} alt="cloudy" className="logo inactive"></img>
-      <img
-        src={logos.logo09_1x}
-        alt="light rain"
-        className="logo inactive"
-      ></img>
-      <img src={logos.logo10_1x} alt="rain" className="logo inactive"></img>
-      <img src={logos.logo11_1x} alt="storm" className="logo inactive"></img>
-      <img src={logos.logo13_1x} alt="snow" className="logo inactive"></img>
-      <img src={logos.logo50_1x} alt="mist" className="logo inactive"></img>
     </div>
   );
 }
