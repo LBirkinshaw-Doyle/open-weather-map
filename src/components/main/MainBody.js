2d1cbef33e478f169ee34445fc56b3c7684d5596
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2022-10-03 09:57:39 +0100

    implement Leaflet map

--- src/components/main/MainBody.js
+++ src/components/main/MainBody.js
@@ -1,5 +1,18 @@
+import { MapContainer, TileLayer } from 'react-leaflet';
+
+import './MainBody.css';
+
 function MainBody() {
-  return <div id="main-body" />;
+  return (
+    <div id="main-body">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
 }
 
 export default MainBody;
